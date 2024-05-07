9878a7a37e1ce6f31ae04a4d0714c9f6624103f2
oksanaliu <164259712+oksanaliu@users.noreply.github.com>
2024-05-07 20:18:16 +0200

    added

---
+++ bot.py
@@ -0,0 +1,54 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError("Phone number must be 10 digits")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if str(p) != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for index, phone in enumerate(self.phones):
+            if str(phone) == old_phone:
+                self.phones[index] = Phone(new_phone)
+                break
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if str(p) == phone:
+                return p
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
